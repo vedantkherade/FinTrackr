d47cfcca9e5f0018348f4adef0416c818d340511
vedantkherade <vedantkherade76@gmail.com>
2026-01-11 16:40:01 +0530

    adding transactions to firebase and fetching them

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,14 +1,25 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import Header from '../components/Header/Header'
 import Cards from '../components/Cards/Cards'
 import { Modal,  } from 'antd';
-
+import dayjs from 'dayjs';
 import AddExpense from '../components/Modals/addExpense.jsx';
 import AddIncome from '../components/Modals/addIncome.jsx';
+import { addDoc, collection, getDocs, query } from 'firebase/firestore';
+import { auth, db } from '../firebase';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { toast } from 'react-toastify';
+
 
 function Dashboard() {
+  const [user] = useAuthState(auth);
   const [isExpenseModalVisible, setIsExpenseModalVisible] = useState(false);
   const [isIncomeModalVisible, setIsIncomeModalVisible] = useState(false);
+  const [transactions, setTransactions] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [currentBalance, setCurrentBalance] = useState(0);
+  const [income, setIncome] = useState(0);
+  const [expenses, setExpenses] = useState(0);
 
   const showExpenseModal = () => {
     setIsExpenseModalVisible(true);
@@ -26,31 +37,84 @@ function Dashboard() {
     setIsIncomeModalVisible(false);
   };
   
+ 
   const onFinish = (values, type) => {
-   console.log("On Finish", values, type);
+    const newTransaction = {
+      type: type,
+      date: dayjs(values.date).format("YYYY-MM-DD"),
+      amount: parseFloat(values.amount),
+      tag: values.tag,
+      name: values.name,
+    };
+
+    addTransaction(newTransaction);
+
   };
-  
+
+ async function addTransaction(transaction) {
+    try {
+      const docRef = await addDoc(
+        collection(db, `users/${user.uid}/transactions`),
+        transaction
+      );
+      console.log("Document written with ID: ", docRef.id);
+      toast.success("Transaction Added!");
+    
+    } catch (e) {
+      console.error("Error adding document: ", e);
+      toast.error("Couldn't add transaction");
+      
+    }
+  }
+
+  useEffect(() => {
+    fetchTransactions();
+  }, []);
+
+  async function fetchTransactions() {
+    setLoading(true);
+    if (user) {
+      const q = query(collection(db, `users/${user.uid}/transactions`));
+      const querySnapshot = await getDocs(q);
+      let transactionsArray = [];
+      querySnapshot.forEach((doc) => {
+        // doc.data() is never undefined for query doc snapshots
+        transactionsArray.push(doc.data());
+      });
+      setTransactions(transactionsArray);
+      console.log("Transactions Array", transactionsArray)
+      toast.success("Transactions Fetched!");
+    }
+    setLoading(false);
+  }
+
+
   return (
     <div>
       <Header />
-      <Cards
-        showExpenseModal={() => setIsExpenseModalVisible(true)}
-        showIncomeModal={() => setIsIncomeModalVisible(true)}
-      />
 
-      <AddExpense
-        isExpenseModalVisible={isExpenseModalVisible}
-        handleExpenseCancel={() => setIsExpenseModalVisible(false)}
-        onFinish={onFinish}
-      />
+      {loading ? (
+        <p>Loading...</p>
+      ) : (
+        <>
+          <Cards
+            showExpenseModal={() => setIsExpenseModalVisible(true)}
+            showIncomeModal={() => setIsIncomeModalVisible(true)}
+          />
 
-      <AddIncome
-        isIncomeModalVisible={isIncomeModalVisible}
-        handleIncomeCancel={() => setIsIncomeModalVisible(false)}
-        onFinish={onFinish}
-      />
-     
-      
+          <AddExpense
+            isExpenseModalVisible={isExpenseModalVisible}
+            handleExpenseCancel={() => setIsExpenseModalVisible(false)}
+            onFinish={onFinish}
+          />
+
+          <AddIncome
+            isIncomeModalVisible={isIncomeModalVisible}
+            handleIncomeCancel={() => setIsIncomeModalVisible(false)}
+            onFinish={onFinish}
+          />
+        </>
+      )}
     </div>
   );
 }
